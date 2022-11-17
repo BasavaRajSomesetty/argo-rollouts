1954d8f261fb92d7a6a73ab10220d8ef0111ef2a
Veera Venkata Satyanarayana Telu <satya@opsmx.io>
2022-11-17 16:28:25 +0530

    Update rollout.tsx

--- ui/src/app/components/rollout/rollout.tsx
+++ ui/src/app/components/rollout/rollout.tsx
@@ -100,6 +100,8 @@ export const RolloutWidget = (props: {rollout: RolloutRolloutInfo; interactive?:
     const passDataToRollout = (data:any) => {
         setreport(data.showReports);
         setData(data);
+        const divElement = document.getElementById('report-bg');
+        divElement.scrollIntoView({ behavior: 'smooth' });
       }
     for (const img of images) {
         for (const container of rollout.containers || []) {
@@ -112,7 +114,7 @@ export const RolloutWidget = (props: {rollout: RolloutRolloutInfo; interactive?:
     return (
         <React.Fragment>
                 {showreport && 
-               <ThemeDiv className='info reports__info'>
+               <ThemeDiv className='report-bg reports__info'>
                    <ReportsWidget clickback={()=> setreport(false)} reportsInput={revisionData}/>
                 </ThemeDiv>}
             <div className='rollout__row rollout__row--top'>
